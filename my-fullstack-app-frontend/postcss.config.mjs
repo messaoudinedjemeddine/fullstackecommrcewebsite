eeaab3831e60129db8f3a000db88176ce0b9bf03
Messaoudi Nedjem Eddine <messaoudinedjemeddine@benflismotors.net>
2025-06-16 17:01:58 +0100

    Deleted tsconfig.json

--- my-fullstack-app-frontend/postcss.config.mjs
+++
@@ -1,8 +0,0 @@
-// postcss.config.mjs
-// Correct configuration for PostCSS plugins, including Tailwind CSS v4.
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
